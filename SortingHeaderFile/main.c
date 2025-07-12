d7b05900ebecca46fd9fb572e1668ae481ca23a0
Biraj-Bhatta <iambirajbhatta@gmail.com>
2025-07-12 18:42:29 +0545

    cahnged in main.c

--- SortingHeaderFile/main.c
+++ SortingHeaderFile/main.c
@@ -6,17 +6,28 @@ int main(){
     int arr[8] = {1, 12, 45, 56, 67, 100, 99,88};
     int size = sizeof(arr) / sizeof(arr[0]);
     int size_of_array=sizeof(array)/sizeof(array[0]);
+  
+    printf("mergesort:\n");
     printf("Before Sorting:\n");
     display(arr, size);
     mergesort(arr,0,size);
     printf("After Sorting:\n");
     display(arr, size);
     
+    printf("\nheapesort:\n");
     printf("Before Sorting: \n");
     display(array,size_of_array);
     heapsort(array,size_of_array);
     printf("After Sorting: \n");
     display(array,size_of_array);
 
+    printf("\nquicksort:\n");
+    printf("Before Sorting: \n");
+    display(array,size_of_array);
+    quicksort(array,size_of_array);
+    printf("After Sorting: \n");
+    display(array,size_of_array);
+
+   
     return 0;
 }
