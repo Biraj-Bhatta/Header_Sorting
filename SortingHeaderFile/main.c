a77fe9be3758ffbb7e8ca150d09f831701d2742d
Biraj-Bhatta <iambirajbhatta@gmail.com>
2025-07-15 22:42:15 +0545

    comment

--- SortingHeaderFile/main.c
+++ SortingHeaderFile/main.c
@@ -1,33 +1,33 @@
-#include <stdio.h>
 #include "sort.h"
+#include <stdio.h>
 
-int main(){
-    int array[10]={1,2,3,45,54,32,7,78,90,100};
-    int arr[8] = {1, 12, 45, 56, 67, 100, 99,88};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int size_of_array=sizeof(array)/sizeof(array[0]);
-  
-    printf("mergesort:\n");
-    printf("Before Sorting:\n");
-    display(arr, size);
-    mergesort(arr,0,size);
-    printf("After Sorting:\n");
-    display(arr, size);
-    
-    printf("\nheapesort:\n");
-    printf("Before Sorting: \n");
-    display(array,size_of_array);
-    heapsort(array,size_of_array);
-    printf("After Sorting: \n");
-    display(array,size_of_array);
+int main() {
+  int array[10] = {1, 2, 3, 45, 54, 32, 7, 78, 90, 100};
+  int arr[8] = {1, 12, 45, 56, 67, 100, 99, 88};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  int size_of_array = sizeof(array) / sizeof(array[0]);
 
-    printf("\nquicksort:\n");
-    printf("Before Sorting: \n");
-    display(array,size_of_array);
-    quicksort(array,size_of_array);
-    printf("After Sorting: \n");
-    display(array,size_of_array);
+  printf("mergesort:\n");
+  printf("Before Sorting:\n");
+  display(arr, size);
+  mergesort(arr, 0, size);
+  printf("After Sorting:\n");
+  display(arr, size);
 
-   
-    return 0;
+  printf("\nheapesort:\n");
+  printf("Before Sorting: \n");
+  display(array, size_of_array);
+  heapsort(array, size_of_array);
+  printf("After Sorting: \n");
+  display(array, size_of_array);
+
+  printf("\nquicksort:\n");
+  printf("Before Sorting: \n");
+  display(array, size_of_array);
+  quicksort(array, size_of_array);
+  printf("After Sorting: \n");
+  display(array, size_of_array);
+
+  // just to check github ssh
+  return 0;
 }
