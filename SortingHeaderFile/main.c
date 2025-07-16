cc473b8146cd5504d859dc1aafd0e0414d3f07d8
Biraj-Bhatta <iambirajbhatta@gmail.com>
2025-07-16 08:01:32 +0545

    added make file

--- SortingHeaderFile/main.c
+++ SortingHeaderFile/main.c
@@ -3,7 +3,10 @@
 
 int main() {
   int array[10] = {1, 2, 3, 45, 54, 32, 7, 78, 90, 100};
+  int newarr[5] = {45, 32, 1, 23, 100};
   int arr[8] = {1, 12, 45, 56, 67, 100, 99, 88};
+
+  int new_size = sizeof(newarr) / sizeof(newarr[0]);
   int size = sizeof(arr) / sizeof(arr[0]);
   int size_of_array = sizeof(array) / sizeof(array[0]);
 
@@ -23,11 +26,10 @@ int main() {
 
   printf("\nquicksort:\n");
   printf("Before Sorting: \n");
-  display(array, size_of_array);
-  quicksort(array, size_of_array);
+  display(newarr, new_size);
+  quicksort(newarr, new_size);
   printf("After Sorting: \n");
-  display(array, size_of_array);
+  display(newarr, new_size);
 
-  // just to check github ssh
   return 0;
 }
